955528f00f1df70732bb7f936230af83bb73cec1
Chi-Min Oh <sapeyes@gmail.com>
2011-08-10 11:58:11 +0900

    LRF empty class created

---
+++ include/devices/LRF.h
@@ -0,0 +1,17 @@
+/*
+	CNU, IIMIL
+	Chi-Min Oh
+
+	Created in June, 2011 by Chi-Min Oh (sapeyes@image.chonnam.ac.kr)
+*/
+
+#ifndef RECOGBOT_DEVICES_LRF_H
+#define RECOGBOT_DEVICES_LRF_H
+
+namespace Recogbot {
+	class LRF {
+	public:
+		
+	};
+}
+#endif

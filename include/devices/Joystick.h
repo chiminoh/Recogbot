3991b2792badca4340a71b321bb6d74add2338db
JueSungMun <tjdansld87@naver.com>
2011-08-10 16:36:36 +0900

    This is my first test by sung mun

---
+++ include/devices/Joystick.h
@@ -0,0 +1,17 @@
+/*
+	CNU, IIMIL
+	Sung-Mun Jue
+
+	Created in August, 2011 by Sung-Mun Jue (tjdansld87@naver.com)
+*/
+
+#ifndef RECOGBOT_DEVICES_JOYSTICK_H
+#define RECOGBOT_DEVICES_JOYSTICK_H
+
+namespace Recogbot {
+	class Joystick {
+	public:
+	
+	};
+}
+#endif

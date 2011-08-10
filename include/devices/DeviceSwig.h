c84fc171f87b968fa0ef144ae245e2e1a1e52fbb
Yongcheol Lee <budlbaram@nate.com>
2011-08-11 04:44:21 +0900

    DeviceSwig empty class create...

---
+++ include/devices/DeviceSwig.h
@@ -0,0 +1,17 @@
+/*
+	CNU, IIMIL
+	Yong-Cheol Lee
+
+	Created in August, 2011 by Sung-Mun Jue (tjdansld87@naver.com)
+*/
+
+#ifndef RECOGBOT_DEVICES_DEVICESWIG_H
+#define RECOGBOT_DEVICES_DEVICESWIG_H
+
+namespace Recogbot {
+	class DeviceSwig {
+	public:
+	
+	};
+}
+#endif

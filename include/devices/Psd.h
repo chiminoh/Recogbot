9eb7a8032d91832e6149e1b856d927495b9f6024
Dae Young Kim <kdy5213@nate.com>
2011-08-11 10:12:09 +0900

    Psd file created

---
+++ include/devices/Psd.h
@@ -0,0 +1,17 @@
+/*
+	CNU, IIMIL
+	DaeYoung Kim
+
+	Created in Aug, 2011 by DaeYoung Kim (kdy5213@nate.com)
+*/
+
+#ifndef RECOGBOT_DEVICES_PSD_H
+#define RECOGBOT_DEVICES_PSD_H
+
+namespace Recogbot {
+	class PSD {
+	public:
+		
+	};
+}
+#endif
